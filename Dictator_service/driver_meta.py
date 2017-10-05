26d9744583ea2d7e931fe4f2571cd659d0572220
sidanbu <sidanbu@users.noreply.github.com>
2017-10-05 12:29:04 +0530

    Update driver_meta.py

--- Dictator_service/driver_meta.py
+++ Dictator_service/driver_meta.py
@@ -115,7 +115,7 @@ class Driver:
 	
 		try:
 			self.method_id="Init_connection()"
-			self.con=MySQLdb.connect("localhost","root","a2mated@P4l4d10n","nmapscan")
+			self.con=MySQLdb.connect("localhost","<USER>","<PASSWORD>","nmapscan")
 			self.cursor = self.con.cursor()
 		except Exception,ee:
 			self.print_Error("EXception in connection-->"+str(ee))
